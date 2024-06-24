7a72d83613f8fa3c14ecbed27860ecc28642db3e
RandomElo <eloi.random@gmail.com>
2024-06-24 19:58:08 +0200

    Test Github Action 13

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import path from "path";
 import cors from "cors";
-import bodyParser from "body-parser";
+//import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 
 import { accesibiliteBDD } from "./middlewares/accesibiliteBDD.js";
@@ -26,8 +26,8 @@ app.use(
         credentials: true, //Les informations d'authorisation doivent être envoyées lors de la demande de cross origini
     })
 );
-app.use(bodyParser.json({ limit: "10mb" }));
-app.use(bodyParser.urlencoded({ extended: true, limit: "10mb" }));
+//app.use(bodyParser.json({ limit: "10mb" }));
+//app.use(bodyParser.urlencoded({ extended: true, limit: "10mb" }));
 
 app.use(express.json()); //Permet d'analyser le corps des requetes entrantes au format JSON
 app.use("/", express.static(path.join(process.cwd(), "public")));
