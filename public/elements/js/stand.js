27ff1480714c4e4d9fff0a3b49be24532287bcb7
RandomElo <eloi.random@gmail.com>
2024-06-26 13:15:26 +0200

    Correction caméra mobile 24

--- public/elements/js/stand.js
+++ public/elements/js/stand.js
@@ -122,37 +122,33 @@ async function gestionScan(contenu) {
 scanner.addListener("scan", gestionScan); // Ajouter un écouteur pour le scan des QR codes
 
 // Obtenir la liste des caméras et les ajouter au select
-
 Instascan.Camera.getCameras()
     .then(function (cameras) {
-        cameras.forEach((camera, i) => {
-            var option = document.createElement("option");
-            option.value = camera.id;
-            option.text = camera.name || `Caméra ${i + 1}`;
-            cameraSelect.appendChild(option);
-        });
+        if (cameras.length > 0) {
+            cameras.forEach((camera, i) => {
+                var option = document.createElement("option");
+                option.value = camera.id;
+                option.text = camera.name || `Caméra ${i + 1}`;
+                cameraSelect.appendChild(option);
+            });
 
-        // Ajouter un écouteur sur le changement de sélection de caméra
-        cameraSelect.addEventListener("change", () => {
-            var selectedCameraId = cameraSelect.value;
-            demarrerVideo(selectedCameraId);
-            scanner.start(selectedCameraId);
-        });
+            // Sélectionner la deuxième caméra si elle existe
+            var selectedCameraIndex = cameras.length > 1 ? 1 : 0;
+            var selectedCamera = cameras[selectedCameraIndex];
+            cameraSelect.value = selectedCamera.id;
 
-        // Démarrer avec la première caméra disponible
-        if (cameras.length == 1) {
-            cameraSelect.value = cameras[0].id;
-            demarrerVideo(cameras[0].id);
-            scanner.start(cameras[0]);
-        } else if (cameras.length == 2) {
-            requeteCamera("Je suis dans la deuxieme catégorie de longueur");
-            requeteCamera(cameras);
-            requeteCamera(cameras[1]);
-            cameraSelect.value = cameras[1].id;
-            demarrerVideo(cameras[1].id);
-            scanner.start(cameras[1]);
+            // Démarrer la vidéo et le scanner avec la caméra sélectionnée
+            demarrerVideo(selectedCamera.id);
+            scanner.start(selectedCamera);
+
+            // Ajouter un écouteur sur le changement de sélection de caméra
+            cameraSelect.addEventListener("change", () => {
+                var selectedCameraId = cameraSelect.value;
+                demarrerVideo(selectedCameraId);
+                scanner.start(cameras.find((camera) => camera.id === selectedCameraId));
+            });
         } else {
-            requeteCamera("Je suis pas dans un catégorie de longeur");
+            console.error("Aucune caméra trouvée");
         }
     })
     .catch(function (erreur) {
